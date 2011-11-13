00a5f2627af77a740d213777a7d74794a779a012
Roman Prokofyev <roman.prokofyev@gmail.com>
2011-11-13 22:25:53 +0100

    jenkins runner added

---
+++ django_selenium/jenkins_runner.py
@@ -0,0 +1,19 @@
+from django_jenkins.runner import CITestSuiteRunner
+from django_selenium.selenium_runner import SeleniumTestRunner
+
+class MyJenkinsRunner(CITestSuiteRunner, SeleniumTestRunner):
+    def __init__(self, **kwargs):
+        super(MyJenkinsRunner, self).__init__(**kwargs)
+        self.selenium = True
+
+    def build_suite(self, test_labels, **kwargs):
+        suite = SeleniumTestRunner.build_suite(self, test_labels, **kwargs)
+        return suite
+
+    def run_tests(self, test_labels, extra_tests=None, **kwargs):
+
+        self._start_selenium()
+        results = super(MyJenkinsRunner, self).run_tests(test_labels, extra_tests, **kwargs)
+        self._stop_selenium()
+
+        return results
