19f5aceb875a6d410c3ed1b3cb1bb5a99280efb6
Roman Prokofyev <roman.prokofyev@gmail.com>
2011-12-04 17:48:58 +0100

    update version and authors

--- django_selenium/__init__.py
+++ django_selenium/__init__.py
@@ -2,7 +2,7 @@
 
 __author__      = 'Roman Prokofyev'
 __license__     = 'Apache License 2.0'
-__version__     = '0.5'
+__version__     = '0.7'
 __maintainer__  = __author__
 __email__       = 'roman.prokofyev@gmail.com'
 __url__         = 'https://github.com/dragoon/django-selenium/'
