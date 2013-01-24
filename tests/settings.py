ac6149f877d7c22e1b45fe63aab4c1308c8b5136
Roman Prokofyev <roman.prokofyev@gmail.com>
2013-01-24 08:45:09 +0100

    add proper display to fix tests

--- tests/settings.py
+++ tests/settings.py
@@ -4,13 +4,7 @@ import os
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
-)
-
 PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
-
-MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
@@ -105,30 +99,6 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
-
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
-
 # ----------------- SELENIUM ----------------------
-SELENIUM_DRIVER = 'Chrome'
-
+SELENIUM_DRIVER = 'Firefox'
+SELENIUM_DISPLAY = ":99.0"
