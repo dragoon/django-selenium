56f857d3ed1313d72def004b09b2fa5e2faf4a7f
Roman Prokofyev <roman.prokofyev@gmail.com>
2013-02-20 17:41:56 +0100

    add unit test for SeleniumElement

--- tests/core/tests.py
+++ tests/core/tests.py
@@ -1,5 +1,18 @@
+import unittest
+
 from django.core.urlresolvers import reverse
 from django_selenium.livetestcases import SeleniumLiveTestCase
+from django_selenium.testcases import SeleniumElement
+
+
+class SimpleUnitTests(unittest.TestCase):
+
+    def test_multiple_elements(self):
+        test_list = ['one string', 'another string']
+        se = SeleniumElement(test_list)
+        self.assertEquals(se.replace('one', 'two'), 'two string')
+        for i, elem in enumerate(se):
+            self.assertEquals(elem, test_list[i])
 
 
 class MyTestCase(SeleniumLiveTestCase):
